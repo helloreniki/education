d7280682c7708ab55b0b7f6095ad30a9cf13304f
Reniki <hello@reniki.com>
2022-07-08 21:03:47 +0200

    add approved column to pivot table

--- database/migrations/2022_07_08_150133_create_education_user_table.php
+++ database/migrations/2022_07_08_150133_create_education_user_table.php
@@ -17,6 +17,7 @@ return new class extends Migration
             $table->id();
             $table->foreignId('education_id')->constrained()->cascadeOnDelete();
             $table->foreignId('user_id')->constrained()->cascadeOnDelete();
+            $table->boolean('approved')->nullable();
             $table->timestamps();
         });
     }
