b9f7077f56958fcf6d5e9835ae9d0815ae52c0c4
Reniki <hello@reniki.com>
2022-07-24 13:57:04 +0200

    db seeder: roles, permissions

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -44,28 +44,45 @@ class DatabaseSeeder extends Seeder
             'email' => 'renata@email.com',
         ]);
 
-        $moderator = User::factory()->create([
-            'name' => 'Moderator'
+        $direktor = User::factory()->create([
+            'name' => 'Direktor'
+        ]);
+
+        $tajnica = User::factory()->create([
+            'name' => 'Tajnica'
         ]);
 
         $simple_users = User::factory(30)->create();
 
-        $roles = ['admin', 'moderator', 'user'];
+        $roles = ['admin', 'editor', 'user'];
         foreach ($roles as $role) {
             Role::factory()->create(['name' => $role]);
         }
 
-        $admin_role = Role::find(1);
+        // attach roles to users: admin, direktor, tajnica
+        $admin->roles()->attach(1);
+        $direktor->roles()->attach(1);
+        $tajnica->roles()->attach(2);
 
-        $admin->roles()->attach(Role::find(1));
-        $moderator->roles()->attach(Role::find(2));
+        foreach ($simple_users as $user) {
+            $user->roles()->attach(3);
+        }
 
-        $permissions = ['can_create_user', 'can_delete_user', 'can_create_education'];
+        $permissions = ['manage_roles', 'approve', 'manage_users', 'manage_educations', 'manage_departments', 'see_own_educations'];
         foreach ($permissions as $permission) {
             Permission::factory()->create(['name' => $permission]);
         }
 
+        // attach all Permission::all too admin_role
+        $admin_role = Role::find(1);
         $admin_role->permissions()->attach(Permission::select('id')->get());
+
+        $editor_role = Role::find(2);
+        $editor_role->permissions()->attach([3,4,5]);
+
+        $user_role = Role::find(3);
+        $user_role->permissions()->attach(6);
+
 
         $organisers = ['lekarniska zbornica', 'sfd', 'zdravniska zbornica', 'farmapro', 'doctrina', 'interno'];
         foreach ($organisers as $organiser) {
